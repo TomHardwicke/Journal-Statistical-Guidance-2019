a99dc057921561d498ae4f3ee5256b6830078db0
Tom Hardwicke <hardwickete@gmail.com>
2021-07-20 17:30:42 +0200

    Create rawToPrimary.R
    script to convert raw data to primary data

---
+++ analysis/rawToPrimary.R
@@ -0,0 +1,19 @@
+# convert raw data to primary data
+
+library(tidyverse)
+library(here)
+
+# Load raw (Google drive) data
+
+d_coding <- read_csv(here("data","raw","Statistical guidance for authors coding form 2.0 (Responses) - Form Responses.csv")) # load raw data
+d_coding <- d_coding %>% select(-Timestamp, -`If relevant, use this space to note anything unusual about this journal that the team needs to discuss.`) # remove timestamps, remove notes
+write_csv(d_coding,here("data","primary","data_coding.csv")) # save file to primary data folder
+
+d_journals <- read_csv(here("data","raw","data_journals.csv")) # load raw data
+# no changes necessary
+write_csv(d_coding,here("data","primary","data_journals.csv")) # save file to primary data folder
+
+
+
+
+
