3542da8edf959b341212e8b6ee8dfb99b1a9658b
Tom Hardwicke <hardwickete@gmail.com>
2021-08-10 16:09:37 +0200

    removing column during raw to primary conversion

--- analysis/rawToPrimary.R
+++ analysis/rawToPrimary.R
@@ -6,11 +6,12 @@ library(here)
 # Load raw (Google drive) data
 
 d_coding <- read_csv(here("data","raw","Statistical guidance for authors coding form 2.0 (Responses) - Form Responses.csv")) # load raw data
-d_coding <- d_coding %>% select(-Timestamp, -`If relevant, use this space to note anything unusual about this journal that the team needs to discuss.`) # remove timestamps, remove notes
+d_coding <- d_coding %>% 
+  select(-Timestamp, -`If relevant, use this space to note anything unusual about this journal that the team needs to discuss.`) # remove timestamps, remove notes
 write_csv(d_coding,here("data","primary","data_coding.csv")) # save file to primary data folder
 
-d_journals <- read_csv(here("data","raw","statGuide_journalList - PPPR_journalList.csv")) # load raw data
-# no changes necessary
+d_journals <- read_csv(here("data","raw","statGuide_journalList - PPPR_journalList.csv")) %>% # load raw data
+  select(-`TH check`) # remove column
 write_csv(d_journals,here("data","primary","data_journals.csv")) # save file to primary data folder
 
 
