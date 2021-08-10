8f351c1a9baf0e473a28125965de3ff4185fca24
Tom Hardwicke <hardwickete@gmail.com>
2021-08-10 15:32:04 +0200

    fix for direct download of journals data

--- analysis/rawToPrimary.R
+++ analysis/rawToPrimary.R
@@ -9,7 +9,7 @@ d_coding <- read_csv(here("data","raw","Statistical guidance for authors coding 
 d_coding <- d_coding %>% select(-Timestamp, -`If relevant, use this space to note anything unusual about this journal that the team needs to discuss.`) # remove timestamps, remove notes
 write_csv(d_coding,here("data","primary","data_coding.csv")) # save file to primary data folder
 
-d_journals <- read_csv(here("data","raw","data_journals.csv")) # load raw data
+d_journals <- read_csv(here("data","raw","statGuide_journalList - PPPR_journalList.csv")) # load raw data
 # no changes necessary
 write_csv(d_journals,here("data","primary","data_journals.csv")) # save file to primary data folder
 
